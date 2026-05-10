95cadfc2b34a04deed02ecd4e7c65e2c557c322e
tildearrow <me@tildearrow.org>
2026-05-09 20:04:14 -0500

    GBA DMA: use DivPitchTable

--- src/engine/platform/gbadma.h
+++ src/engine/platform/gbadma.h
@@ -86,6 +86,7 @@ class DivPlatformGBADMA: public DivDispatch {
     void notifyInsChange(int ins);
     void notifyWaveChange(int wave);
     void notifyInsDeletion(void* ins);
+    void notifyPitchTable(int sample=-1);
     const void* getSampleMem(int index = 0);
     size_t getSampleMemCapacity(int index = 0);
     size_t getSampleMemUsage(int index = 0);
